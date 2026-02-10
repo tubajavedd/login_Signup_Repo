4f5537e9170151f3ee404ccf2d8b9d10bd47b08e
Tuba Javed <javedtuba1@example.com>
2026-02-10 20:22:32 +0000

    add profile edit/updation code in adminProfile

--- vaidyaGo/settings.py
+++ vaidyaGo/settings.py
@@ -60,12 +60,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'feedback',
     'accounts',
-<<<<<<< HEAD
     'adminProfile',
-=======
      'rest_framework',
     'rest_framework.authtoken',
->>>>>>> 66dc246b9b305a1f29323fd5068dcc63ab2b12c9
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -182,10 +179,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
-<<<<<<< HEAD
 }
-=======
-}
+
+
 AUTH_USER_MODEL = 'accounts.User'
 
->>>>>>> d057b919f4b6153a3a692750aedcfa1a879db74b
